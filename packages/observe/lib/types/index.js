c1e48bd083ea6fed022582895a088a53c5b4e18e
v8-embedding <289700172@qq.com>
2026-08-15 23:49:37 +0800

    add observe plugin (timeline filters/groups, stats panel, live settings config)

---
+++ packages/observe/lib/types/index.js
@@ -0,0 +1,3 @@
+/** Host loader entry: the gateway class itself is the plugin (class shape). */
+export { ObserveGateway as default, ObserveGateway } from "./host/index.js";
+//# sourceMappingURL=index.js.map

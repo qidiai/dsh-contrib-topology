2dcf1ad3bf63623d04d7153b29fc375c775a55c3
v8-embedding <289700172@qq.com>
2026-08-15 23:04:37 +0800

    sync latest source + built lib (group partitioning, dashed disabled edges, prepublish gate)

---
+++ packages/topology/lib/types/index.js
@@ -0,0 +1,3 @@
+/** Host loader entry: the gateway class itself is the plugin (class shape). */
+export { TopologyGateway as default, TopologyGateway } from "./host/index.js";
+//# sourceMappingURL=index.js.map

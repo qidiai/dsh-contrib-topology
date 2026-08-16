b4ac8944399bd531ccb25457555722e53a74570b
v8-embedding <289700172@qq.com>
2026-08-16 18:07:00 +0800

    chore: remove polluted no-slash path

--- packagesroutersrcindex.ts
+++
@@ -1,4 +0,0 @@
-/** Host loader entry: the router gateway class itself is the plugin (class shape). */
-
-export { RouterGateway as default, RouterGateway } from './host/index.ts'
-export type * from './types.ts'

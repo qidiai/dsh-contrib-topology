fba47a180736e7a74e8ff250592ebf44961c7948
v8-embedding <289700172@qq.com>
2026-08-16 10:32:33 +0800

    fix: remove polluted bridge types paths (audit cleanup)

--- packagesmcp-bridgelibtypesindex.js
+++
@@ -1,3 +0,0 @@
-/** Host loader entry: the bridge gateway class itself is the plugin (class shape). */
-export { McpBridgeGateway as default, McpBridgeGateway } from "./host/index.js";
-//# sourceMappingURL=index.js.map

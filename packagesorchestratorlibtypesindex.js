86a6b4d7ef158dc08c3e74c0f35021e623a0b3de
v8-embedding <289700172@qq.com>
2026-08-16 18:05:22 +0800

    chore: remove polluted no-slash path

--- packagesorchestratorlibtypesindex.js
+++
@@ -1,3 +0,0 @@
-/** Host loader entry: the orchestrator gateway class itself is the plugin (class shape). */
-export { OrchestratorGateway as default, OrchestratorGateway } from "./host/index.js";
-//# sourceMappingURL=index.js.map

4bfd9ced80366c495ed8646a849e2d0725dd9572
v8-embedding <289700172@qq.com>
2026-08-16 18:05:20 +0800

    chore: remove polluted no-slash path

--- packagesorchestratorlibtypesindex.d.ts
+++
@@ -1,4 +0,0 @@
-/** Host loader entry: the orchestrator gateway class itself is the plugin (class shape). */
-export { OrchestratorGateway as default, OrchestratorGateway } from './host/index.ts';
-export type * from './types.ts';
-//# sourceMappingURL=index.d.ts.map

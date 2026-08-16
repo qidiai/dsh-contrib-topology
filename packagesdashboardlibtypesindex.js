f64feaecb8c46b3b3b2abfe4d912c4ded5ba3755
v8-embedding <289700172@qq.com>
2026-08-16 18:03:35 +0800

    chore: remove polluted dashboard path (no-slash bug)

--- packagesdashboardlibtypesindex.js
+++
@@ -1,3 +0,0 @@
-/** Host loader entry: the dashboard gateway class itself is the plugin (class shape). */
-export { DashboardGateway as default, DashboardGateway } from "./host/index.js";
-//# sourceMappingURL=index.js.map

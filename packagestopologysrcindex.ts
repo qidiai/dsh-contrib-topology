bb9409c63359fb4fbf823e948c932b98210ed2de
v8-embedding <289700172@qq.com>
2026-08-16 18:08:18 +0800

    chore: remove polluted no-slash path

--- packagestopologysrcindex.ts
+++
@@ -1,4 +0,0 @@
-/** Host loader entry: the gateway class itself is the plugin (class shape). */
-
-export { TopologyGateway as default, TopologyGateway } from './host/index.ts'
-export type * from './types.ts'
